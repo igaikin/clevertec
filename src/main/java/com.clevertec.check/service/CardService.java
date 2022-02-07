a930cfb0405c9a86f46d5eae3014b80aae9abb83
Gaikin <gaikin@icloud.com>
2022-02-07 22:03:44 +0300

    add CardService interface

---
+++ src/main/java/com.clevertec.check/service/CardService.java
@@ -0,0 +1,4 @@
+package com.clevertec.check.service;
+
+public interface CardService {
+}
--- src/main/java/com.clevertec.check/service/impl/CardServiceImpl.java
+++ src/main/java/com.clevertec.check/service/impl/CardServiceImpl.java
@@ -4,10 +4,11 @@ import com.clevertec.check.bean.Card;
 import com.clevertec.check.dao.CardDao;
 import com.clevertec.check.dao.impl.CardDaoFileImpl;
 import com.clevertec.check.exception.CheckException;
+import com.clevertec.check.service.CardService;
 
 import java.util.Optional;
 
-public class CardServiceImpl {
+public class CardServiceImpl implements CardService {
     private static final CardDao cardDao = new CardDaoFileImpl();
 
     public static Card getCard(long id) throws RuntimeException {
