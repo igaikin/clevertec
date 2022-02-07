514a24ee8ee340500eecd544d72f5af545129a3b
Gaikin <gaikin@icloud.com>
2022-02-07 08:43:50 +0300

    add Lombok annotation for Check and nested classes

--- src/main/java/com.clevertec.check/bean/Check.java
+++ src/main/java/com.clevertec.check/bean/Check.java
@@ -1,47 +1,23 @@
 package com.clevertec.check.bean;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class Check {
     private final List<Line> lines = new ArrayList<>();
     private TotalLine totalLine;
     private boolean isOnPromo;
     private BigDecimal promoDiscount;
 
-    public BigDecimal getPromoDiscount() {
-        return promoDiscount;
-    }
-
-    public void setPromoDiscount(BigDecimal promoDiscount) {
-        this.promoDiscount = promoDiscount;
-    }
-
-    public boolean isOnPromo() {
-        return isOnPromo;
-    }
-
-    public void setOnPromo(boolean onPromo) {
-        isOnPromo = onPromo;
-    }
-
-    public List<Line> getLines() {
-        return lines;
-    }
-
     public void addLine(Line line) {
         lines.add(line);
     }
 
-    public TotalLine getTotalLine() {
-        return totalLine;
-    }
-
-    public void setTotalLine(TotalLine totalLine) {
-        this.totalLine = totalLine;
-    }
-
+    @Data
     public static class Line {
         private int quantity;
         private String description;
@@ -50,7 +26,6 @@ public class Check {
         private boolean promo;
         private BigDecimal total;
 
-
         public Line(int quantity, Product product, BigDecimal total, boolean promo) {
             this.quantity = quantity;
             this.description = product.getDescription();
@@ -58,56 +33,9 @@ public class Check {
             this.total = total;
             this.promo = promo;
         }
-
-        public BigDecimal getPromoCost() {
-            return promoCost;
-        }
-
-        public void setPromoCost(BigDecimal promoCost) {
-            this.promoCost = promoCost;
-        }
-
-        public int getQuantity() {
-            return quantity;
-        }
-
-        public void setQuantity(int quantity) {
-            this.quantity = quantity;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public BigDecimal getFullCost() {
-            return fullCost;
-        }
-
-        public void setFullCost(BigDecimal fullCost) {
-            this.fullCost = fullCost;
-        }
-
-        public boolean isPromo() {
-            return promo;
-        }
-
-        public void setPromo(boolean promo) {
-            this.promo = promo;
-        }
-
-        public BigDecimal getTotal() {
-            return total;
-        }
-
-        public void setTotal(BigDecimal total) {
-            this.total = total;
-        }
     }
 
+    @Data
     public static class TotalLine {
         private BigDecimal discountPercent;
         private BigDecimal discountAmount;
@@ -116,30 +44,6 @@ public class Check {
         public TotalLine(BigDecimal discountPercent, BigDecimal discountAmount, BigDecimal totalCost) {
             this.discountPercent = discountPercent;
             this.discountAmount = discountAmount;
-            this.totalCost = totalCost;
-        }
-
-        public BigDecimal getDiscountPercent() {
-            return discountPercent;
-        }
-
-        public void setDiscountPercent(BigDecimal discountPercent) {
-            this.discountPercent = discountPercent;
-        }
-
-        public BigDecimal getDiscountAmount() {
-            return discountAmount;
-        }
-
-        public void setDiscountAmount(BigDecimal discountAmount) {
-            this.discountAmount = discountAmount;
-        }
-
-        public BigDecimal getTotalCost() {
-            return totalCost;
-        }
-
-        public void setTotalCost(BigDecimal totalCost) {
             this.totalCost = totalCost;
         }
     }
