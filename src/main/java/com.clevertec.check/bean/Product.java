be678b0fa2c85a0b105de4886c4960dd2044a158
Gaikin <gaikin@icloud.com>
2022-02-07 08:42:31 +0300

    add Lombok annotation for Product

--- src/main/java/com.clevertec.check/bean/Product.java
+++ src/main/java/com.clevertec.check/bean/Product.java
@@ -1,50 +1,16 @@
 package com.clevertec.check.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
 public class Product {
 
     private long id;
     private String description;
     private BigDecimal cost;
     private boolean isOnPromo;
-
-    public Product(long id, String description, BigDecimal cost, boolean promo) {
-        this.id = id;
-        this.description = description;
-        this.cost = cost;
-        this.isOnPromo = promo;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getCost() {
-        return cost;
-    }
-
-    public void setCost(BigDecimal cost) {
-        this.cost = cost;
-    }
-
-    public boolean isOnPromo() {
-        return isOnPromo;
-    }
-
-    public void setOnPromo(boolean onPromo) {
-        this.isOnPromo = onPromo;
-    }
 }
