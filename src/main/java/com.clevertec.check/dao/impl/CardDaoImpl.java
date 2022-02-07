7bee42ec3e8d839aa12ffa71d3c99f3918620c9d
Gaikin <gaikin@icloud.com>
2022-02-07 20:54:10 +0300

    added CRUD for map

--- src/main/java/com.clevertec.check/dao/impl/CardDaoImpl.java
+++ src/main/java/com.clevertec.check/dao/impl/CardDaoImpl.java
@@ -4,7 +4,9 @@ import com.clevertec.check.bean.Card;
 import com.clevertec.check.dao.CardDao;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 public class CardDaoImpl implements CardDao {
@@ -17,7 +19,34 @@ public class CardDaoImpl implements CardDao {
         CARDS.put(3333L, new Card(3333, BigDecimal.valueOf(7.0)));
     }
 
+    @Override
+    public Card create(Card card) {
+        CARDS.put(card.getId(), card);
+        return card;
+    }
+
+    @Override
+    public List<Card> getAll(Card card) {
+        List<Card> cards = new ArrayList<>();
+        cards.add(card);
+        return cards;
+    }
+
+    @Override
     public Optional<Card> get(long id) {
         return Optional.of(CARDS.get(id));
+    }
+
+    @Override
+    public Card update(Card card) {
+        Card updateCard = CARDS.get(card.getId());
+        updateCard.setDiscount(card.getDiscount());
+        return updateCard;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        CARDS.remove(id);
+        return true;
     }
 }
