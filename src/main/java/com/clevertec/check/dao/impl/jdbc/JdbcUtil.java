a813b09d0ce4ef6ed0ca2e5e5c1ca9e56c8d4e38
Gaikin <gaikin@icloud.com>
2022-02-19 20:40:35 +0300

    add jdbcUtil

---
+++ src/main/java/com/clevertec/check/dao/impl/jdbc/JdbcUtil.java
@@ -0,0 +1,25 @@
+package com.clevertec.check.dao.impl.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class JdbcUtil {
+
+    public static void checkRowsUpdated(int rowsUpdated, int expectedRows) {
+        if (rowsUpdated < expectedRows) {
+            throw new RuntimeException("Not updated");
+        } else if (rowsUpdated > expectedRows) {
+            throw new RuntimeException("...");//FIXME message
+        }
+    }
+
+    public static void setAutoCommitTrue(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.setAutoCommit(true);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();//FIXME logger!
+        }
+    }
+}
